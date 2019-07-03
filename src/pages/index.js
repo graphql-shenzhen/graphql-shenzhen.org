58fd27d52f5b01deac239a148e7f2070e8ced009
Tobias Meixner <meixner.tobias@gmail.com>
2019-07-03 08:52:48 +0800

    footer content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1437,7 +1437,7 @@ const IndexPage = () => (
             </ul>
           </div>
           <div className="col-md-6 col-lg-3 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.6s">
-            <h3>RECENT POSTS</h3>
+            {/* <h3>RECENT POSTS</h3>
             <ul className="image-list">
               <li>
                 <figure className="overlay">
@@ -1457,10 +1457,10 @@ const IndexPage = () => (
                   <div className="meta"><span className="date">October 12, 2018</span></div>
                 </div>
               </li>
-            </ul>
+            </ul> */}
           </div>
           <div className="col-md-6 col-lg-3 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.8s">
-            <h3>SUBSCRIBE US</h3>
+            {/* <h3>SUBSCRIBE US</h3>
             <div className="widget">
               <div className="newsletter-wrapper">
                 <form method="post" id="subscribe-form" name="subscribe-form" className="validate">
@@ -1471,15 +1471,15 @@ const IndexPage = () => (
                   </div>
                 </form>
               </div>
-            </div>
+            </div> */}
             {/* <!-- /.widget --> */}
             <div className="widget">
               <h5 className="widget-title">FOLLOW US ON</h5>
               <ul className="footer-social">
-                <li><a className="facebook" href="#"><i className="lni-facebook-filled"></i></a></li>
+                {/* <li><a className="facebook" href="#"><i className="lni-facebook-filled"></i></a></li> */}
                 <li><a className="twitter" href="#"><i className="lni-twitter-filled"></i></a></li>
-                <li><a className="linkedin" href="#"><i className="lni-linkedin-filled"></i></a></li>
-                <li><a className="google-plus" href="#"><i className="lni-google-plus"></i></a></li>
+                {/* <li><a className="linkedin" href="#"><i className="lni-linkedin-filled"></i></a></li> */}
+                {/* <li><a className="google-plus" href="#"><i className="lni-google-plus"></i></a></li> */}
               </ul>
             </div>
           </div>
