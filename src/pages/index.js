910a4a7f0dafedafe5fc66d66e4f0fd754f879ee
Carlos Rufo <sw@carlosrj.com>
2019-08-28 12:22:44 +0800

    add bart-verkoeijen

--- src/pages/index.js
+++ src/pages/index.js
@@ -542,6 +542,39 @@ const IndexPage = () => (
           <div className="col-sm-6 col-md-6 col-lg-4">
             {/* <!-- Team Item Starts --> */}
             <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
+              <a href="https://twitter.com/bgever">
+                <div className="team-img">
+                  <img className="img-fluid" src={require('../images/bart-verkoeijen.png')} alt="Bart Verkoeijen" />
+                  <div className="team-overlay">
+                    <div className="overlay-social-icon text-center">
+                      <ul className="social-icons">
+                        <li><a href="https://twitter.com/bgever"><i className="lni-twitter-filled" aria-hidden="true"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="info-text">
+                  <h3><a href="https://twitter.com/bgever">Bart Verkoeijen</a></h3>
+                  <p>Developer Advocate, KintoHub</p>
+                </div>
+              </a>
+            </div>
+            {/* <!-- Team Item Ends --> */}
+          </div>
+        </div>
+        <br/><br/>
+        <div className="row">
+          <div className="col-12">
+            <div className="section-title-header text-center">
+              <h1 className="section-title wow fadeInUp" data-wow-delay="0.2s">Organizers</h1>
+              {/* <p className="wow fadeInDown" data-wow-delay="0.2s">Global Grand Event on Digital Design</p> */}
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center align-items-center">
+          <div className="col-sm-6 col-md-6 col-lg-4">
+            {/* <!-- Team Item Starts --> */}
+            <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
               <a href="https://twitter.com/codingcoop">
                 <div className="team-img">
                   <img className="img-fluid" src={require('../images/joseph-cooper.png')} alt="Joseph Cooper" />
